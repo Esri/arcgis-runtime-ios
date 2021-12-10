bcc3de49cf47102c2e1eb2ec791c90cbf14d239e
Zachary Kline <zkline@esri.com>
2021-12-10 12:49:05 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -38,12 +38,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ArcGIS",
-            url: "https://www.dropbox.com/s/h2awbdmdl5uetb6/ArcGIS-iOS-v100.13.xcframework.zip?dl=1",
+            url: "https://developers.arcgis.com/ios/downloads/ArcGIS-iOS-v100.13.xcframework.zip?dl=1",
             checksum: "98231f876f18954fe5426750b2910002f6942a3d6d8fff862abcdff17525f2eb"
         ),
         .binaryTarget(
             name: "Runtimecore",
-            url: "https://www.dropbox.com/s/jdych5ev8102t39/Runtimecore-v100.13.xcframework.zip?dl=1",
+            url: "https://developers.arcgis.com/ios/downloads/Runtimecore-v100.13.xcframework.zip?dl=1",
             checksum: "3a1948f5ac8b2a4b6c57b02cd15e57bca468e534cd0ca490a4f29337a6eae968"
         )
     ]
